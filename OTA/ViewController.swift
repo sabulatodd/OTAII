6e47e496489dfcbdb6fe961ce1952d40e6336d4f
Todd Papke <sabulatodd@mac.com>
2015-09-17 15:29:29 -0500

    Initial Commit

---
+++ OTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTA
+//
+//  Created by Todd Papke on 9/17/15.
+//  Copyright © 2015 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OTATests/OTATests.swift
@@ -0,0 +1,36 @@
+//
+//  OTATests.swift
+//  OTATests
+//
+//  Created by Todd Papke on 9/17/15.
+//  Copyright © 2015 Todd Papke. All rights reserved.
+//
+
+import XCTest
+@testable import OTA
+
+class OTATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
