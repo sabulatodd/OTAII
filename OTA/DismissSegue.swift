bb062c43ef9b956afd67582943a88e5c161bbf0a
Todd Papke <sabulatodd@mac.com>
2015-09-17 19:24:36 -0500

    segue

---
+++ OTA/DismissSegue.swift
@@ -0,0 +1,16 @@
+////
+////  DismissSegue.swift
+////  OTA
+////
+////  Created by Todd Papke on 9/17/15.
+////  Copyright © 2015 Todd Papke. All rights reserved.
+////
+//
+//import UIKit
+//
+//class DismissSegue: NSObject {
+//    func perform() {
+//    UIViewController.sourceViewController = self.ViewController
+//        //  sourceViewController.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+//
+//}
