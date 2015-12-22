d51e06ba68d3ede8b4341d1d102a9b7012201693
Todd Papke <sabulatodd@mac.com>
2015-12-22 14:51:38 -0600

    Test

---
+++ OTA/RecommendationsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OrtViewController.swift
+//  OTA
+//
+//  Created by Todd Papke on 10/10/15.
+//  Copyright © 2015 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+
+
+class RecommendationsViewController: UIViewController{
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("RecommendationsViewController")
+        //load initial URL
+           }
+    
+   }
+
